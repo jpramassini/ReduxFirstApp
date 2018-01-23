7994fbf14ad25b952fa5dd93bc33e222fcc9ea2a
jpramassini <jpramassini@gmail.com>
2018-01-22 22:06:44 -0500

    Finished Book Viewer App - Learning Redux.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export function selectBook(book) {
   // selectBook is an ActionCreator, it needs to return and action,
   // an object with a type property.
   return {
-
+      type: 'BOOK_SELECTED',
+      payload: book
   };
 }
